0cdc576dd4f0afb44d1a4d80bf0cae130ad08eb8
mcandra11 <candrariskicandra2@gmail.com>
2024-03-14 04:47:00 +0700

    first commit

---
+++ src/main/java/com/mycompany/helloworld202357201038/HelloWorld202357201038.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld202357201038;
+
+/**
+ *
+ * @author HP
+ */
+public class HelloWorld202357201038 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
